76848817a66f04a8629d85b95cbb50c2d13c3899
SzaZoa1227 <116081320+SzaZoa1227@users.noreply.github.com>
2023-02-28 18:03:00 +0000

    sadf

--- mukodjmar.js
+++ mukodjmar.js
@@ -1,19 +1,22 @@
 function hatter() {
+    let igen = 1;
     let x = document.getElementsByClassName("js");
-    let hatterszin = x.style.backgroundColor;
+    let hatterszin = x.style.background-color;
     let border = x.style.border;
-    let arnyek = x.stlye.boxShadow;
+    let arnyek = x.stlye.box-shadow;
     let betuszin = x.style.color;
-    if (x.style.backgroundColor = "rgba(255, 255, 255, 1)") {
+    if (igen = 1) {
       border = "none";
       hatterszin = "rgba(255, 255, 255, 0)";
       betuszin = "rgba(0,0,0, 0)";
       arnyek = "none";
+      int = 0;
     }
     else {
       border = "1px solid #ccc";
       hatterszin = "rgba(255, 255, 255, 1)";
       betuszin = "#000";
       arnyek = "0 0 5px #ccc";
+      int = 1;
     };
   }
